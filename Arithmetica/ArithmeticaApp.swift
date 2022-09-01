198c68f59490ae637acb90d57659205c4e11ab6d
maxmerc17 <maxmercer36@gmail.com>
2022-09-01 13:40:41 -0400

    Initial Commit

---
+++ Arithmetica/ArithmeticaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticaApp.swift
+//  Arithmetica
+//
+//  Created by Max Mercer on 2022-09-01.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
